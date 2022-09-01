38ce16d4643e52614c32eaabcbae579a430c4843
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2022-09-01 00:49:49 -0300

    Remocao do loader + aplicacao da api de traucao + adicao de algumas informacoes sobre o retorno da api

--- src/script.js
+++ src/script.js
@@ -3,15 +3,20 @@ const key = "fR8GLAeajZ0hUjoZHHByoUz1cJInfxl6GpKG9pCg";
 
 const completeUrl = url + key
 function getUser() {
-    document.getElementById('banner-day').insertAdjacentHTML('beforeend',`<p id="loading-message">Loading</p>`)
     axios.get(completeUrl)
     .then(response=>{
         document.getElementById('explanation').innerHTML = response.data.explanation;
         document.getElementById('banner-day').style.backgroundImage = `url(${response.data.hdurl})`
-        document.getElementById("loading-message").remove()
+        document.getElementById('apod-name').innerHTML = response.data.title;
+        document.getElementById('apod-date').innerHTML = response.data.date;
+
         console.log(response);
     })
     .catch(error=>console.log(error))
 }
 
 getUser()
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
