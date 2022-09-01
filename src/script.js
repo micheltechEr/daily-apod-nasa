1373c61ac9958da04bede9e04abb8dff90a0292a
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2022-09-01 00:00:33 -0300

    Testando loader

--- src/script.js
+++ src/script.js
@@ -3,10 +3,12 @@ const key = "fR8GLAeajZ0hUjoZHHByoUz1cJInfxl6GpKG9pCg";
 
 const completeUrl = url + key
 function getUser() {
+    document.getElementById('banner-day').insertAdjacentHTML('beforeend',`<p id="loading-message">Loading</p>`)
     axios.get(completeUrl)
     .then(response=>{
         document.getElementById('explanation').innerHTML = response.data.explanation;
         document.getElementById('banner-day').style.backgroundImage = `url(${response.data.hdurl})`
+        document.getElementById("loading-message").remove()
         console.log(response);
     })
     .catch(error=>console.log(error))
